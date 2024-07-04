6112682b41f897da418d78c7d644262741f01a02
Shahwat Hasnaine <shahwat.hasnaine@enosisbd.com>
2024-07-04 11:04:49 +0600

    Initialize Application

---
+++ Pjsip_Sample_App/Pjsip_Sample_App/Pjsip_Sample_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pjsip_Sample_AppApp.swift
+//  Pjsip_Sample_App
+//
+//  Created by Shahwat Hasnaine on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Pjsip_Sample_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
